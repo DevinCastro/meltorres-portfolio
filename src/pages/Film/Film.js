19e86ac8480658cc977805c3dc56880e853f3cf5
DevinCastro <dcrocker21@gmail.com>
2020-11-29 11:18:43 -0800

    embeded youtube videos

--- src/pages/Film/Film.js
+++ src/pages/Film/Film.js
@@ -10,9 +10,34 @@ const Film = () => {
       <section>
 
         <Container>
-          <h1>films</h1>
+          <h1>Video Portfolio</h1>
 
 
+          <div className="space"></div>
+
+          <Row>
+            <Col md="6">
+              <h3>Product Demos:</h3>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-rULeXL4gXPCiaLNWLWGEHOW_WEBkQpi" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Col>
+            <Col md="6">
+              <h3>Playthroughs:</h3>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-rULeXL4gXOUMAh4bXLr5oSebT532Qr-" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="6">
+              <h3>Music Videos:</h3>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-rULeXL4gXMKGDkiTHffLi79KhEQWH4B" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Col>
+            <Col md="6">
+              <h3>Corporate Videos</h3>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-rULeXL4gXMCG0nEMidCfYJd4tVXp9MI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Col>
+          </Row>
+
+
+          <div className="space"></div>
 
         </Container>
       </section>
